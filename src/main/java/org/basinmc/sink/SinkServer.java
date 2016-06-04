5efb3b1a8f6fdaf22ff503a32bd4e518ba62e657
Johannes Donath <johannesd@torchmind.com>
2016-06-04 10:35:46 +0200

    Added a server configuration API and implementation.

--- src/main/java/org/basinmc/sink/SinkServer.java
+++ src/main/java/org/basinmc/sink/SinkServer.java
@@ -17,8 +17,12 @@
 package org.basinmc.sink;
 
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.dedicated.DedicatedServer;
 
 import org.basinmc.faucet.Server;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import javax.annotation.Nonnull;
 
@@ -26,9 +30,10 @@ import javax.annotation.Nonnull;
  * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
  */
 public class SinkServer implements Server {
-    private final MinecraftServer server;
+    private final DedicatedServer server;
+    private final Server.Configuration configuration = new Configuration();
 
-    public SinkServer(MinecraftServer server) {
+    public SinkServer(DedicatedServer server) {
         this.server = server;
     }
 
@@ -47,5 +52,217 @@ public class SinkServer implements Server {
     @Override
     public boolean isOnlineMode() {
         return this.server.isServerInOnlineMode();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nonnull
+    @Override
+    public Path getBaseDirectory() {
+        return Paths.get(".");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nonnull
+    @Override
+    public Server.Configuration getConfiguration() {
+        return this.configuration;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int getLifeTime() {
+        return this.server.getTickCounter();
+    }
+
+    private class Configuration implements Server.Configuration {
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean areAchievementAnnouncementsEnabled() {
+            return SinkServer.this.server.isAnnouncingPlayerAchievements();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean areCommandBlocksEnabled() {
+            return SinkServer.this.server.isCommandBlockEnabled();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Nonnull
+        @Override
+        public String getHostname() {
+            return SinkServer.this.server.getServerHostname();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public int getMaximumConcurrentPlayers() {
+            return SinkServer.this.server.getMaxPlayers();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public int getMaximumBuildHeight() {
+            return SinkServer.this.server.getBuildLimit();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public long getMaximumTickTime() {
+            return SinkServer.this.server.getMaxTickTime();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public int getNetworkCompressionThreshold() {
+            return SinkServer.this.server.getNetworkCompressionThreshold();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public int getOperatorPermissionLevel() {
+            return SinkServer.this.server.getOpPermissionLevel();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public int getPlayerIdleTimeout() {
+            return SinkServer.this.server.getSettings().getIntProperty("player-idle-timeout", 0);
+        }
+
+        @Override
+        public int getQueryPort() {
+            return SinkServer.this.server.getSettings().getIntProperty("query.port", 25565);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Nonnull
+        @Override
+        public String getRemoteConsolePassword() {
+            return SinkServer.this.server.getSettings().getStringProperty("rcon.password", "");
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public int getRemoteConsolePort() {
+            return SinkServer.this.server.getSettings().getIntProperty("rcon.port", 25575);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public int getSpawnProtectionRadius() {
+            return SinkServer.this.server.getSpawnProtectionSize();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public int getViewDistance() {
+            return SinkServer.this.server.getSettings().getIntProperty("view-distance", 10);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean isFlightAllowed() {
+            return SinkServer.this.server.isFlightAllowed();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean isGamemodeForced() {
+            return SinkServer.this.server.getForceGamemode();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean isHardcore() {
+            return SinkServer.this.server.isHardcore();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean isNativeTransportEnabled() {
+            return SinkServer.this.server.shouldUseNativeTransport();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean isNetherAllowed() {
+            return SinkServer.this.server.getAllowNether();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean isPvpEnabled() {
+            return SinkServer.this.server.isPVPEnabled();
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean isRemoteConsoleEnabled() {
+            return SinkServer.this.server.getSettings().getBooleanProperty("enable-rcon", false);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean isQueryEnabled() {
+            return SinkServer.this.server.getSettings().getBooleanProperty("enable-query", false);
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public boolean isSnooperEnabled() {
+            return SinkServer.this.server.isSnooperEnabled();
+        }
     }
 }
