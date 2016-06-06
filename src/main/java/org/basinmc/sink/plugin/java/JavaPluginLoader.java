8ffdfa16b28dcd229e6c68aeed23015b9dcac547
Johannes Donath <johannesd@torchmind.com>
2016-06-06 13:16:14 +0200

    Moved Java plugin loader to a more appropriate directory.

--- src/main/java/org/basinmc/sink/plugin/java/JavaPluginLoader.java
+++ src/main/java/org/basinmc/sink/plugin/java/JavaPluginLoader.java
@@ -1,21 +1,20 @@
 /*
- *  Copyright 2016 __0x277F <0x277F@gmail.com>
- *  and other copyright owners as documented in the project's IP log.
+ * Copyright 2016 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License&quot￼;
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * 	http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-package org.basinmc.sink.plugin;
+package org.basinmc.sink.plugin.java;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
