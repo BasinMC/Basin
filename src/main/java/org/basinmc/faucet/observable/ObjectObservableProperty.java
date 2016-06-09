8f25f0481e12d0e7ddfddf179d680d22e09abbbc
Johannes Donath <johannesd@torchmind.com>
2016-06-09 21:47:47 +0200

    Implemented a basic observable for regular objects.

---
+++ src/main/java/org/basinmc/faucet/observable/ObjectObservableProperty.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2016 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.basinmc.faucet.observable;
+
+/**
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+public class ObjectObservableProperty<T> extends AbstractObservableProperty<T> {
+    private T value;
+
+    public ObjectObservableProperty(T initialValue) {
+        this.value = initialValue;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public T get() {
+        return this.value;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean set(T value) {
+        if (this.callObservers(value)) {
+            this.value = value;
+            return true;
+        }
+
+        return false;
+    }
+}
