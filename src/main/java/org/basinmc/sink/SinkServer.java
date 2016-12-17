fdff9f497d9cba8e10c734221d3c1ef9853770a4
Johannes Donath <johannesd@torchmind.com>
2016-12-17 02:30:24 +0100

    Removed unused call to Collection#stream()

--- src/main/java/org/basinmc/sink/SinkServer.java
+++ src/main/java/org/basinmc/sink/SinkServer.java
@@ -128,8 +128,7 @@ public class SinkServer implements Server, Handled<DedicatedServer> {
     public void shutdown(@Nullable String reason) {
         // TODO Fire shutdown event here, once I add it. :)
         server.logInfo("Server Shutdown: " + reason);
-        server.getPlayerList().getPlayerList().stream()
-                .forEach(player -> player.connection.kickPlayerFromServer(reason));
+        server.getPlayerList().getPlayerList().forEach(player -> player.connection.kickPlayerFromServer(reason));
         server.stopServer();
     }
 
