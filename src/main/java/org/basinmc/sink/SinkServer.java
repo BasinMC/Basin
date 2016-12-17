6a72d3bb85ef4c51df1181265a6c7b2dc20f5d80
Johannes Donath <johannesd@torchmind.com>
2016-12-17 05:49:28 +0100

    Integrated the OSGi framework (lacking auto deploy).

--- src/main/java/org/basinmc/sink/SinkServer.java
+++ src/main/java/org/basinmc/sink/SinkServer.java
@@ -18,21 +18,41 @@ package org.basinmc.sink;
 
 import com.google.common.collect.Iterators;
 
-import net.minecraft.server.dedicated.DedicatedServer;
+import com.mojang.authlib.GameProfileRepository;
+import com.mojang.authlib.minecraft.MinecraftSessionService;
+import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
 
-import org.apache.felix.framework.util.FelixConstants;
+import net.minecraft.init.Bootstrap;
+import net.minecraft.server.dedicated.DedicatedServer;
+import net.minecraft.server.management.PlayerProfileCache;
+import net.minecraft.util.datafix.DataFixesManager;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.basinmc.faucet.BasinVersion;
 import org.basinmc.faucet.Handled;
 import org.basinmc.faucet.Server;
+import org.osgi.framework.BundleException;
 import org.osgi.framework.Constants;
 import org.osgi.framework.launch.Framework;
 import org.osgi.framework.launch.FrameworkFactory;
 
+import java.awt.*;
+import java.io.File;
+import java.net.Proxy;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.ServiceLoader;
+import java.util.UUID;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -41,11 +61,27 @@ import javax.annotation.Nullable;
  * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
  */
 public class SinkServer implements Server, Handled<DedicatedServer> {
+    private static final Logger logger = LogManager.getFormatterLogger(SinkServer.class);
+    private static final Options OPTIONS = new Options()
+            .addOption(Option.builder().longOpt("help").desc("Prints this help message before gracefully shutting the application down.").build())
+            .addOption(Option.builder("b").longOpt("bundle-directory").hasArg().desc("Declares the directory to store plugin and library bundles in.").build())
+            .addOption(Option.builder("c").longOpt("cache-directory").hasArg().desc("Declares the directory to store framework cache files in.").build())
+            .addOption(Option.builder().longOpt("enable-development-features").desc("Enables features which are designed to assist developers with creating plugins.").build())
+            .addOption(Option.builder().longOpt("disable-easter-eggs").desc("Disables all easter eggs which have been introduced by the Basin team.").build())
+            .addOption(Option.builder().longOpt("disable-gui").desc("Prevents the server GUI to be displayed (this setting is automatically activated in headless environments).").build())
+            .addOption(Option.builder().longOpt("disable-performance-optimizations").desc("Disables all Sink performance optimizations.").build())
+            .addOption(Option.builder().longOpt("server-port").hasArg().desc("Declares the port this server should listen on (defaults to 25565, may be overridden in server.properties)").build())
+            .addOption(Option.builder().longOpt("version").desc("Prints the application and API version before gracefully shutting the application down.").build())
+            .addOption(Option.builder("w").longOpt("world-directory").hasArg().desc("Declares the directory to store the worlds of this server in (default: worlds).").build());
+
+    // this is pretty ugly but it'll contribute to keeping our patches a little smaller
+    private static SinkServer instance;
+
     private final DedicatedServer server;
     private final Server.Configuration configuration = new Configuration();
     private final Framework framework;
 
-    public SinkServer(@Nonnull DedicatedServer server) throws ClassNotFoundException, IllegalStateException {
+    private SinkServer(@Nonnull DedicatedServer server) throws ClassNotFoundException, IllegalStateException {
         this.server = server;
         this.framework = buildFrameworkInstance();
 
@@ -122,6 +158,96 @@ public class SinkServer implements Server, Handled<DedicatedServer> {
     }
 
     /**
+     * Provides a replacement entry point for the server in order to gain complete control over the
+     * command line arguments passed to Sink.
+     *
+     * This method is to be kept in sync with
+     * {@link net.minecraft.server.MinecraftServer#main(String[])} to maintain compatibility.
+     *
+     * @param arguments an array of command line arguments.
+     */
+    public static void main(@Nonnull String[] arguments) {
+        try {
+            main(new DefaultParser().parse(OPTIONS, arguments));
+        } catch (ParseException ex) {
+            System.err.println("Invalid Input: " + ex.getMessage());
+            System.err.println();
+            printHelp();
+            System.exit(-1);
+        }
+    }
+
+    /**
+     * Provides a simplified internal entry point to decouple command line parsing from the actual
+     * main method implementation.
+     */
+    private static void main(@Nonnull CommandLine cmd) {
+        // since this output is intended to be used by bash scripts or other third party software
+        // in order to detect compatibility, we'll give this argument the highest possible priority
+        if (cmd.hasOption("version")) {
+            System.out.println(BasinVersion.MINECRAFT_VERSION);
+            System.out.println(BasinVersion.API_VERSION);
+            return;
+        }
+
+        System.out.println("Basin Sink for Minecraft " + BasinVersion.MINECRAFT_VERSION + " (implementing Faucet " + BasinVersion.API_VERSION + ")");
+        System.out.println("Provided under the terms of the Apache License, Version 2.0");
+
+        // print the help message before starting the server since we won't need its capabilities
+        // in order to notify users of our very own functionality
+        if (cmd.hasOption("help")) {
+            printHelp();
+            System.exit(0);
+        }
+
+        logger.info("Initializing Minecraft %s", BasinVersion.MINECRAFT_VERSION);
+        Bootstrap.register(); // apparently this is how the registries work ... don't question it
+
+        // log some environment information
+        logger.info("Running on Java v%s supplied by %s", System.getProperty("java.version", "Unknown"), System.getProperty("java.vendor"));
+
+        if (cmd.hasOption("disable-gui") || GraphicsEnvironment.isHeadless()) {
+            logger.info("Server GUI has been disabled or is unavailable in this environment");
+        }
+
+        // TODO: We probably want to replace some of these implementations in order to allow plugins
+        // to properly replace or alter their behavior
+        YggdrasilAuthenticationService var15 = new YggdrasilAuthenticationService(Proxy.NO_PROXY, UUID.randomUUID().toString());
+        MinecraftSessionService var16 = var15.createMinecraftSessionService();
+        GameProfileRepository var17 = var15.createProfileRepository();
+        PlayerProfileCache var18 = new PlayerProfileCache(var17, new File(".", "usercache.json"));
+
+        DedicatedServer server = new DedicatedServer(new File("."), DataFixesManager.createFixer(), var15, var16, var17, var18);
+        server.setFolderName(cmd.hasOption("world-directory") ? "world-directory" : "world");
+
+        if (cmd.hasOption("server-port")) {
+            server.setServerPort(Integer.parseUnsignedInt(cmd.getOptionValue("server-port")));
+        }
+
+        // TODO: Bonus chests?
+
+        if (!cmd.hasOption("disable-gui") && !GraphicsEnvironment.isHeadless()) {
+            server.setGuiEnabled();
+        } else {
+            logger.info("Server GUI has been disabled or is not available within the current environment");
+        }
+
+        try {
+            instance = new SinkServer(server);
+            instance.start();
+        } catch (ClassNotFoundException ex) {
+            throw new RuntimeException("Invalid Class-Path: " + ex.getMessage(), ex);
+        }
+    }
+
+    /**
+     * Prints the command line argument help to the standard program output.
+     */
+    private static void printHelp() {
+        new HelpFormatter().printHelp("java -jar Sink.jar", OPTIONS, true);
+    }
+
+    /**
      * {@inheritDoc}
      */
     @Override
@@ -133,11 +259,29 @@ public class SinkServer implements Server, Handled<DedicatedServer> {
     }
 
     /**
+     * Handles the startup phase of the server.
+     */
+    private void start() {
+        try {
+            logger.info("Starting plugin framework");
+            this.framework.start();
+        } catch (BundleException ex) {
+            logger.error("Failed to initialize plugin framework: " + ex.getMessage(), ex);
+            logger.error("Cannot continue - Server is shutting down");
+            System.exit(-1);
+        }
+
+        logger.info("Starting Minecraft Server");
+        this.server.startServerThread();
+    }
+
+    /**
      * {@inheritDoc}
      */
     @Nonnull
     @Override
     public Path getBaseDirectory() {
+        // TODO: Add support for a variable base directory since this might be useful in order to share jars
         return Paths.get(".");
     }
 
