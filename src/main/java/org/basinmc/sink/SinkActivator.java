eb168269fc9d275809aea0b163432a6dfe5a0a4d
Johannes Donath <johannesd@torchmind.com>
2016-12-23 16:01:26 +0100

    Properly implemented logging within OSGi.

--- src/main/java/org/basinmc/sink/SinkActivator.java
+++ src/main/java/org/basinmc/sink/SinkActivator.java
@@ -16,6 +16,8 @@
  */
 package org.basinmc.sink;
 
+import com.google.common.io.ByteStreams;
+
 import com.mojang.authlib.GameProfileRepository;
 import com.mojang.authlib.minecraft.MinecraftSessionService;
 import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
@@ -30,10 +32,18 @@ import org.apache.logging.log4j.Logger;
 import org.basinmc.faucet.FaucetVersion;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
 
 import java.awt.*;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.Proxy;
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.UUID;
 
 import javax.annotation.Nonnull;
@@ -48,6 +58,44 @@ public class SinkActivator implements BundleActivator {
     private static final Logger logger = LogManager.getFormatterLogger(SinkActivator.class);
     private SinkServer server;
 
+    /**
+     * Replaces the PAX logging configuration against a more sane minecraft-like configuration.
+     * TODO: This might be better located in the development bundle and/or flange.
+     */
+    private void configureLogger(@Nonnull BundleContext ctx) throws IOException {
+        // extract configuration to an accessible location
+        File configFile = ctx.getDataFile("log4j2.xml");
+
+        try (InputStream inputStream = this.getClass().getResourceAsStream("/log4j2.xml")) {
+            try (FileOutputStream outputStream = new FileOutputStream(configFile)) {
+                ByteStreams.copy(inputStream, outputStream);
+            }
+        }
+
+        // push config to pax logging
+        ServiceReference<?> serviceReference = ctx.getServiceReference(ConfigurationAdmin.class.getName());
+
+        if (serviceReference == null) {
+            logger.warn("Could not replace logging configuration");
+            logger.warn("You are about to experience a mindboggling amount of log messages!");
+            return;
+        }
+
+        ConfigurationAdmin configurationAdmin = (ConfigurationAdmin) ctx.getService(serviceReference);
+        Configuration configuration = configurationAdmin.getConfiguration("org.ops4j.pax.logging", null);
+        Dictionary<String, Object> properties = configuration.getProperties();
+
+        if (properties == null) {
+            properties = new Hashtable<>();
+        }
+
+        properties.put("org.ops4j.pax.logging.log4j2.config.file", configFile.toString());
+
+        configuration.update(properties);
+        logger.info("Loggers have been adjusted to sane levels.");
+    }
+
+    @Nonnull
     private DedicatedServer constructServerInstance(@Nonnull BundleContext ctx) {
         logger.info("Initializing Minecraft %s", FaucetVersion.API_VERSION);
         Bootstrap.register(); // apparently this is how the registries work ... don't question it
@@ -80,6 +128,8 @@ public class SinkActivator implements BundleActivator {
      */
     @Override
     public void start(@Nonnull BundleContext context) throws Exception {
+        this.configureLogger(context);
+
         this.server = new SinkServer(context, this.constructServerInstance(context));
         this.server.start();
     }
