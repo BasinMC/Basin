20c70be42c48c1d4d8785b591e623367c3f31c00
Hex <hex@hex.lc>
2017-01-16 17:17:36 -0700

    Add some internal API.

---
+++ src/main/java/org/basinmc/faucet/internal/trace/Autotrace.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2017 Hex <hex@hex.lc>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.basinmc.faucet.internal.trace;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotates a method to automatically be added as part of a trace chain. A metatracer
+ * instance is acquired via dependency injection. Loading a class that contains methods with
+ * this annotation will result in class transformation. Calls to the metatracer will be placed
+ * immediately before all return statement.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Autotrace {
+    /**
+     * The type of singleton metatracer which handles this trace chain.
+     */
+    Class<? extends Metatracer> value();
+}
