a829fa833d0aae0018fb2978c449b29a245e4954
Hex <hex@hex.lc>
2017-02-05 01:11:39 -0700

    Add date format.

---
+++ src/main/java/org/basinmc/faucet/command/annotation/DateFormat.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2017 Hex <hex@hex.lc>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.basinmc.faucet.command.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Denotes the parsing system for a given parameter as converting a date string in any format into
+ * a number of any given time unit.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface DateFormat {
+    TimeUnit value() default TimeUnit.SECONDS;
+}
