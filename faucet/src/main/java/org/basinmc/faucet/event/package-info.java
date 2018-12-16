5c1fe548cb3d1fc994a39c0938915d77e88cff79
Johannes Donath <johannesd@torchmind.com>
2018-12-16 19:17:46 +0100

    Mark the entire event API volatile for now as we will probably twiddle its bits a few more times.

---
+++ faucet/src/main/java/org/basinmc/faucet/event/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+
+@Volatile
+package org.basinmc.faucet.event;
+
+import org.basinmc.faucet.internal.warn.Volatile;
