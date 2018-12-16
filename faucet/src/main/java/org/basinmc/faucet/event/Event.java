0e4fdcaaefdf71cd015187ca9c95ab6c6c69d822
Johannes Donath <johannesd@torchmind.com>
2018-12-16 19:31:31 +0100

    Added an event base interface.

---
+++ faucet/src/main/java/org/basinmc/faucet/event/Event.java
@@ -0,0 +1,21 @@
+package org.basinmc.faucet.event;
+
+import org.basinmc.faucet.internal.warn.Volatile;
+
+/**
+ * Marks the annotated type as an event which may be passed to the local event bus.
+ *
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+public interface Event<STATE extends Enum<STATE>> {
+
+  /**
+   * Retrieves the default state for this event's execution context.
+   *
+   * @return an arbitrary state.
+   */
+  @Volatile
+  default STATE getDefaultState() {
+    return null;
+  }
+}
