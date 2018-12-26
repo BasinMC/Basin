bc99dbc2b2453d2c7cf242ec58ee0b9858a4c24a
Johannes Donath <johannesd@torchmind.com>
2018-12-26 12:41:09 +0100

    Implemented an extension class loader and the basics of dependency resolving.

--- sink/src/main/java/org/basinmc/sink/plugin/ExtensionImpl.java
+++ sink/src/main/java/org/basinmc/sink/plugin/ExtensionImpl.java
@@ -18,17 +18,24 @@ package org.basinmc.sink.plugin;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 import java.util.jar.JarFile;
 import org.basinmc.faucet.extension.Extension;
 import org.basinmc.faucet.extension.dependency.ExtensionDependency;
 import org.basinmc.faucet.extension.dependency.ServiceDependency;
 import org.basinmc.faucet.extension.dependency.ServiceReference;
+import org.basinmc.faucet.extension.error.ExtensionContainerException;
 import org.basinmc.faucet.extension.error.ExtensionException;
 import org.basinmc.faucet.extension.error.ExtensionManifestException;
 import org.springframework.context.ApplicationContext;
@@ -47,6 +54,8 @@ public class ExtensionImpl implements AutoCloseable, Extension {
   private final String version;
 
   private Phase phase = Phase.REGISTERED;
+  private List<ExtensionImpl> dependencies = new ArrayList<>();
+  private ExtensionClassLoader classLoader;
   private AnnotationConfigApplicationContext ctx;
 
   ExtensionImpl(@NonNull Path containerPath) throws ExtensionException {
@@ -193,6 +202,29 @@ public class ExtensionImpl implements AutoCloseable, Extension {
   }
 
   /**
+   * Retrieves a list of dependencies which have already been resolved.
+   * @return a list of extensions.
+   */
+  @NonNull
+  public List<ExtensionImpl> getResolvedDependencies() {
+    return Collections.unmodifiableList(this.dependencies);
+  }
+
+  /**
+   * <p>Retrieves the class loader with which this extension is loading its classes and
+   * resources.</p>
+   *
+   * <p>If the plugin is not currently loaded or running, an empty optional will be returned
+   * instead.</p>
+   *
+   * @return an extension class loader.
+   */
+  @NonNull
+  public Optional<ExtensionClassLoader> getClassLoader() {
+    return Optional.ofNullable(this.classLoader);
+  }
+
+  /**
    * <p>Retrieves the context in which this extension is <strong>currently</strong> executed.</p>
    *
    * <p>If the plugin is not in the running phase, an empty optional will be returned instead as no
@@ -205,16 +237,33 @@ public class ExtensionImpl implements AutoCloseable, Extension {
     return Optional.ofNullable(this.ctx);
   }
 
-  public void initialize(@NonNull ApplicationContext parentCtx) {
+  public void initialize() throws ExtensionContainerException {
+    if (this.ctx != null) {
+      return;
+    }
+
+    try {
+      this.classLoader = new ExtensionClassLoader(this); // TODO: Custom URL scheme for extensions?
+    } catch (MalformedURLException ex) {
+      throw new ExtensionContainerException("Failed to open extension container", ex);
+    }
+
+    this.phase = Phase.LOADED;
+  }
+
+  public void start(@NonNull ApplicationContext parentCtx) {
     if (this.ctx != null) {
       return;
     }
 
     this.ctx = new AnnotationConfigApplicationContext();
     this.ctx.setParent(parentCtx);
-    // TODO
+    this.ctx.setClassLoader(this.classLoader);
 
-    this.phase = Phase.LOADED;
+    // TODO: Register service registration beans
+
+    this.ctx.refresh();
+    this.ctx.start();
   }
 
   public void stop() {
