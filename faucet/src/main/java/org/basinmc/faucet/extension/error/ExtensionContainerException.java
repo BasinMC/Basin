b0da148b0267417aecbab2c0936854e0784db32d
Johannes Donath <johannesd@torchmind.com>
2018-12-26 12:40:34 +0100

    Created an exception to express container issues.

---
+++ faucet/src/main/java/org/basinmc/faucet/extension/error/ExtensionContainerException.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2018 Johannes Donath  <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.basinmc.faucet.extension.error;
+
+/**
+ * Notifies callers about issues with the container location or format.
+ *
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ * @since 1.0
+ */
+public class ExtensionContainerException extends ExtensionException {
+
+  public ExtensionContainerException() {
+  }
+
+  public ExtensionContainerException(String message) {
+    super(message);
+  }
+
+  public ExtensionContainerException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public ExtensionContainerException(Throwable cause) {
+    super(cause);
+  }
+}
