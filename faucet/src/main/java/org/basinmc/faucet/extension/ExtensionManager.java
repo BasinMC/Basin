b9ecd1508e7313abe70b5610f5d1f2acfff7b1d1
Johannes Donath <johannesd@torchmind.com>
2019-01-13 13:29:35 +0100

    Reformat

--- faucet/src/main/java/org/basinmc/faucet/extension/ExtensionManager.java
+++ faucet/src/main/java/org/basinmc/faucet/extension/ExtensionManager.java
@@ -41,9 +41,9 @@ public interface ExtensionManager {
   /**
    * <p>Retrieves a list of extensions which have been registered with the server.</p>
    *
-   * <p>Note that extensions may reside in different phases depending on when they are registered with
-   * the server and whether their startup completed successfully (failed plugins may retain their
-   * resolved state until removed from the server).</p>
+   * <p>Note that extensions may reside in different phases depending on when they are registered
+   * with the server and whether their startup completed successfully (failed plugins may retain
+   * their resolved state until removed from the server).</p>
    *
    * @return a list of registered extensions.
    */
