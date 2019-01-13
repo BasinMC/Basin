00d95b21cbe6fdba740521a2d95912944773b4b7
Johannes Donath <johannesd@torchmind.com>
2019-01-13 17:35:49 +0100

    [BAS-12] Created an event to notify listeners about extension shutdowns.

---
+++ faucet/src/main/java/org/basinmc/faucet/event/extension/ExtensionShutdownEvent.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2019 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.basinmc.faucet.event.extension;
+
+import edu.umd.cs.findbugs.annotations.NonNull;
+import javax.annotation.Nonnull;
+import org.basinmc.faucet.extension.Extension;
+import org.basinmc.faucet.extension.Extension.Phase;
+
+/**
+ * Notifies listeners about the graceful shutdown of an extension.
+ *
+ * @author <a href="mailto:johannesd@torchmind.com">Johannes Donath</a>
+ */
+public interface ExtensionShutdownEvent<S> extends ExtensionPhaseEvent<S> {
+
+  /**
+   * {@inheritDoc}
+   */
+  @Nonnull
+  @Override
+  default Phase getCurrentPhase() {
+    return Phase.RUNNING;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Nonnull
+  @Override
+  // TODO: Target could also be none if removal is desired
+  default Phase getTargetPhase() {
+    return Phase.REGISTERED;
+  }
+
+  final class Pre extends AbstractExtensionEvent<Void> implements ExtensionShutdownEvent<Void> {
+
+    public Pre(@NonNull Extension extension) {
+      super(extension);
+    }
+  }
+
+  final class Post extends AbstractExtensionEvent<Void> implements ExtensionShutdownEvent<Void> {
+
+    public Post(@NonNull Extension extension) {
+      super(extension);
+    }
+  }
+}
